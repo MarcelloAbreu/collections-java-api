75907faef7cc38a32e81047c5a500f1903e52e72
Marcello Henrique <marcellohenriquean13@gmail.com>
2024-06-10 15:11:43 -0300

    ordenação set

--- src/main/java/set/Ordenacao/Produto.java
+++ src/main/java/set/Ordenacao/Produto.java
@@ -80,7 +80,7 @@ public class Produto implements Comparable<Produto> {
                 + "}";
     }
 
-    class ComparatorPorPreco implements Comparator<Produto> {
+    public static class ComparatorPorPreco implements Comparator<Produto> {
         @Override
         public int compare(Produto p1, Produto p2) {
             return Double.compare(p1.getPreco(), p2.getPreco());
